55d35186cf2a0950d3d113a86d55ab473a8d48a1
andoniripollpro <andoni.ripoll.pro@gmail.com>
2022-06-28 10:48:21 +0200

    3

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace POCConsoleCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Este un programa de consola en .NET Core 3.1");
+            Console.ReadLine();
+        }
+    }
+}
